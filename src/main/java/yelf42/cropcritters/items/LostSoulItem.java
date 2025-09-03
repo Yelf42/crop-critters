9ccdda1ec41c3824c1b3f42a80470e5b926337a4
Tom <yelf42@gmail.com>
2025-09-02 17:32:52 -0700

    LostSoul can bring slimeblock back to life

--- src/main/java/yelf42/cropcritters/items/LostSoulItem.java
+++ src/main/java/yelf42/cropcritters/items/LostSoulItem.java
@@ -6,6 +6,7 @@ import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -35,7 +36,29 @@ public class LostSoulItem extends Item {
         BlockPos blockPos = context.getBlockPos();
         BlockState state = world.getBlockState(blockPos);
         ItemStack itemStack = context.getStack();
+        PlayerEntity playerEntity = context.getPlayer();
 
+        // Create slime
+        if (state.isOf(Blocks.SLIME_BLOCK)) {
+            if (world.random.nextInt(2) == 0) {
+                world.spawnParticles(ParticleTypes.SMOKE, blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5, 10, 0.5, 0.5, 0.5, 0F);
+            } else {
+                world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), Block.NOTIFY_LISTENERS);
+                SlimeEntity slime = EntityType.SLIME.create(world, SpawnReason.SPAWN_ITEM_USE);
+                slime.setSize(2, true);
+                slime.setPosition(blockPos.toBottomCenterPos());
+                world.spawnEntity(slime);
+                world.playSound(null, blockPos, SoundEvents.ENTITY_SLIME_SQUISH, SoundCategory.BLOCKS, 1F, 1F);
+                world.spawnParticles(ParticleTypes.SOUL_FIRE_FLAME, blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5, 10, 0.5, 0.5, 0.5, 0F);
+            }
+            if (playerEntity instanceof ServerPlayerEntity serverPlayerEntity) {
+                Criteria.ITEM_USED_ON_BLOCK.trigger(serverPlayerEntity, blockPos, itemStack);
+            }
+            itemStack.decrement(1);
+            return ActionResult.SUCCESS;
+        }
+
+        // Critter spawning logic
         Optional<Entity> toSpawn = spawnCritter(world, blockPos, state);
         if (toSpawn.isEmpty()) return ActionResult.PASS;
         if (world.random.nextInt(2) == 0) {
@@ -49,7 +72,6 @@ public class LostSoulItem extends Item {
             world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), Block.NOTIFY_LISTENERS);
             toSpawn.get().setPosition(toSpawnAt.toBottomCenterPos());
             world.spawnEntity(toSpawn.get());
-            PlayerEntity playerEntity = context.getPlayer();
             world.playSound(null, blockPos, SoundEvents.ENTITY_ALLAY_AMBIENT_WITH_ITEM, SoundCategory.BLOCKS, 1F, 1F);
             world.spawnParticles(ParticleTypes.SOUL_FIRE_FLAME, blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5, 10, 0.5, 0.5, 0.5, 0F);
             if (playerEntity instanceof ServerPlayerEntity serverPlayerEntity) {
